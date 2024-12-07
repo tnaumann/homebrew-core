c7f2e4e62ad9b0ee337ab327eb02dcb27acef02a
Tristan Naumann <tristan@microsoft.com>
2024-12-07 14:47:44 -0500

    azure-pim-cli 0.8.0 (new formula)

--- Formula/a/azure-pim-cli.rb
+++ Formula/a/azure-pim-cli.rb
@@ -1,8 +1,8 @@
 class AzurePimCli < Formula
   desc "Unofficial CLI for Azure Privileged Identity Management (PIM) roles"
   homepage "https://github.com/demoray/azure-pim-cli"
-  url "https://github.com/demoray/azure-pim-cli/archive/refs/tags/0.7.0.tar.gz"
-  sha256 "730edd638d161f0e81b2e0ce3f479e4ac21a3bb125457b02af60966ac7a607a3"
+  url "https://github.com/demoray/azure-pim-cli/archive/refs/tags/0.8.0.tar.gz"
+  sha256 "7505843a58bf5bd44882a1a4ad646f8afd11327e4709180f11daed0b758ed36b"
   license "MIT"
   head "https://github.com/demoray/azure-pim-cli.git", branch: "main"
 
